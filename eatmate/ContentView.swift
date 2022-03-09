973fa338f875fe45e0b1573e085ba5e2ef5ccd0a
cherrystz <plug4869@gmail.com>
2022-03-09 15:56:55 +0700

    Initial Commit

---
+++ eatmate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eatmate
+//
+//  Created by Cherryst 🍒 on 9/3/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ eatmate/eatmateApp.swift
@@ -0,0 +1,17 @@
+//
+//  eatmateApp.swift
+//  eatmate
+//
+//  Created by Cherryst 🍒 on 9/3/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct eatmateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
