3cbcb4fc197c7513980b4054e920a04b190abb25
phumipatapi <gong51243@gmail.com>
2022-03-24 02:53:08 +0700

    add groupView

---
+++ eatmate/MainView/groupView.swift
@@ -0,0 +1,20 @@
+//
+//  groupView.swift
+//  eatmate
+//
+//  Created by Phumipat Apivansri on 24.03.22.
+//
+
+import SwiftUI
+
+struct groupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct groupView_Previews: PreviewProvider {
+    static var previews: some View {
+        groupView()
+    }
+}
