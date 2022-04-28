d8bde1dca0466b8a091996b3def74e0c1b895c9e
phumipatapi <gong51243@gmail.com>
2022-04-28 11:50:54 +0700

    add aboutus detail

--- eatmate/MainView/AboutUsView.swift
+++ eatmate/MainView/AboutUsView.swift
@@ -11,9 +11,44 @@ struct AboutUsView: View {
     var body: some View {
         FullScreenView{
             NavbarView(  destination: AnyView(SettingView()), title: "Eatmate",showBackButton: true,showMoreButton: false,shadow: 2)
-            VStack{
-                Image("EatMateLogo")
-            }
+//            VStack{
+//                Image("EatMateLogo")
+//                    .resizable()
+//                    .frame(width:200, height: 200)
+//                    .padding(.top,80)
+//                Text("EatMate")
+//                    .font(.nunito(size: 40, weight: .bold))
+//                Text("V 0.0.1")
+//                    .font(.nunito(size: 14, weight: .bold))
+//                    .padding(.bottom,20)
+//                    .foregroundColor(.gray)
+//                Text("Feel bored when eating alone?")
+//                    .font(.nunito(size: 18, weight: .regular))
+//                Text("Let us help you find your soulmate!")
+//                    .font(.nunito(size: 18, weight: .regular))
+//
+//            }
+            GeometryReader { gp in
+                       ZStack {
+                           VStack{
+                                          Image("EatMateLogo")
+                                              .resizable()
+                                              .frame(width:200, height: 200)
+                                              
+                                          Text("EatMate")
+                                              .font(.nunito(size: 40, weight: .bold))
+                                          Text("V 0.0.1")
+                                              .font(.nunito(size: 14, weight: .bold))
+                                              .padding(.bottom,10)
+                                              .foregroundColor(.gray)
+                                          Text("Feel bored when eating alone?")
+                                              .font(.nunito(size: 18, weight: .regular))
+                                          Text("Let us help you find your soulmate!")
+                               .font(.nunito(size: 18, weight: .regular))}
+                               .alignmentGuide(VerticalAlignment.center, computeValue: { $0[.bottom] })
+                               .position(x: gp.size.width / 2, y: gp.size.height / 2)
+                       
+                       }}
         }
     }
 }
